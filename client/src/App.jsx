02aa38145f9f96bb448c3fa5a582b0c4aa0b6e3f
HARSHITBHANUSHALI <harshit.bhanushali22@spit.ac.in>
2024-10-03 19:10:18 +0530

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import TextEditor from './TextEditor'
+import {Route,Routes,Navigate} from 'react-router-dom'
+import {v4 as uuidV4} from 'uuid'
+
+function App() {
+  return (
+    <Routes>
+      <Route path='/' element={<Navigate to={`/documents/${uuidV4()}`}/>}/>
+      <Route path='/documents/:id' element={<TextEditor/>}/>
+    </Routes>
+  )
+}
+
+export default App
